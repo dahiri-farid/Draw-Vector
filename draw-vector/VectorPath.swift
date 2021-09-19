c8cadb3129ac4a3176badc83a604d37ae1ec98c6
Farid Dahiri <farid@beat.no>
2021-09-19 19:29:43 +0300

    Add 'VectorPath'

---
+++ draw-vector/VectorPath.swift
@@ -0,0 +1,16 @@
+//
+//  VectorPath.swift
+//  draw-vector
+//
+//  Created by Farid Dahiri on 19.09.2021.
+//
+
+import Foundation
+import UIKit
+
+class VectorPath {
+    var path = [CGPoint]()
+    var strokeWidth = 1
+    var strokeColor = UIColor.black
+    var fillColor = UIColor.black
+}
